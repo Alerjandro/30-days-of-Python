32762c8c8040977a712e7e98dcdfba6a9e69a0ec
Alerjandro <aleferbohor@gmail.com>
2023-01-19 08:59:34 +0100

    day_1 y day_2

---
+++ day_1/helloworld.py
@@ -0,0 +1,38 @@
+print (3+4)
+print (3-4)
+print (3*4)
+print (3%4)
+print (3/4)
+print (3**4)
+print (3//4)
+
+print ('Alejandro')
+print ('Fernández')
+print ('Spain')
+print ('I am enjoying 30 days of python')
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4J))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Alejandro'))
+print(type('Fernández'))
+print(type('Spain'))
+
+print (14+4)
+print (18/9)
+print (12-8j)
+print ('Im blue')
+True # Is the turtle is sleep? If it it's sleep,then valu is true
+print ('wolf', 'rhino', 'sparrow', 'aligator') # animals
+print (['car', 'computer', 'book'])
+
+{'first_name':'Alejandro',
+'last_name': 'Fernández',
+'country':'Spain',
+'age':'16',
+'is_married':False,
+'skills':['chess', 'basket']}
+
+
---
+++ day_2/variables.py
@@ -0,0 +1,51 @@
+print ('Day 2: 30 Days of python programming')
+first_name = 'Alejandro'
+last_name = 'Fernández'
+full_name = 'Alejandro Fernández Bohórquez'
+country = 'Spain'
+city = 'Jerez'
+age = '16'
+years = '2023'
+is_married = 'False'
+is_true = 'dogs can run'
+is_light_on = 'yes'
+hobbie, pet, favorite_sport = 'chess', 'dog', 'basketball'
+
+print (type (first_name))
+print (type (last_name)) 
+print (type (full_name))
+print (type (country))
+print (type (city))
+print (type (age))
+print (type (years))
+print (type (is_married))
+print (type (is_true))
+print (type (is_light_on))
+print (type (hobbie))
+print (type (pet))
+print (type (favorite_sport))
+
+print (len(first_name))
+print (len(first_name)), (len(last_name))
+num_one = 5
+num_two =4
+total=(num_one+num_two)
+diff=(num_one-num_two)
+product=(num_one*num_two)
+division=(num_one/num_two)
+remainder=(num_one%num_two)
+exp=(num_one**num_two)
+floor_division=(num_one//num_two)
+
+
+area_of_circle=((3.1416*30)**2)
+circum_of_circle=(3.14*(30**2))
+radius = int(input("Give me the radius of a circle"))
+print (3.1416*(30**2))
+
+first_name = input ("What is your first name?")
+last_name = input ("What is your last name?")
+country = input ("Wich is your country")
+age = input ("How old are you?")
+print('Hello, my name is', first_name, last_name,'I am from', country,'and i am', age, 'years old' )
+
