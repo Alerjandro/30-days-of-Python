07ba9e016ec906d553156749cafe25114ca7da36
Alerjandro <aleferbohor@gmail.com>
2023-01-27 10:25:19 +0100

    operaciones

---
+++ day_3/Boolean.py
@@ -0,0 +1,17 @@
+age=16
+int_number=1,73
+
+#convert int to float
+float_number = float(int_number)
+
+(complex (8,3))
+
+base=int(input('mi estimado me podría dar una base la cual desee para el triángulo '))
+altura=int(input('mi estimado me podría dar un altura la cual desee para el triángulo '))
+area_del_triangulo = base*altura/2
+
+a=int(input('mi estimado me podría dar el lado a'))
+b=int(input('mi estimado me podría dar el lado b')) 
+c=int(input('mi estimado me podría dar el lado c')) 
+the_perimeter_of_the_triangle = (a+b+c)
+print(the_perimeter_of_the_triangle)
