348f934f01dbac87571d3b57d1f5ab046e21a29c
Alerjandro <aleferbohor@gmail.com>
2023-02-17 10:26:37 +0100

    garbanzo

--- day_3/Boolean.py
+++ day_3/Boolean.py
@@ -1,13 +1,13 @@
 age=16
-int_number=1,73
-
+int_number=1.73
 #convert int to float
-float_number = float(int_number)
+float_number = (int_number)
+print (float_number)
 
 (complex (8,3))
 
-base=int(input('mi estimado me podría dar una base la cual desee para el triángulo '))
-altura=int(input('mi estimado me podría dar un altura la cual desee para el triángulo '))
+base = int(input('mi estimado me podría dar una base la cual desee para el triángulo'))
+altura=int(input('mi estimado me podría dar un altura la cual desee para el triángulo'))
 area_del_triangulo = base*altura/2
 
 a=int(input('mi estimado me podría dar el lado a'))
@@ -15,3 +15,38 @@ b=int(input('mi estimado me podría dar el lado b'))
 c=int(input('mi estimado me podría dar el lado c')) 
 the_perimeter_of_the_triangle = (a+b+c)
 print(the_perimeter_of_the_triangle)
+
+lenght=int(input("mi estimado me podía dar el largo del rectangulo"))
+widht=int(input("mi estimado me podría dar el ancho del triangulo"))
+area_del_rectangulo = lenght*widht
+perimetro_del_rectangulo = 2*(lenght+widht)
+
+radio=int(input("mi estimado me podría dar el radio del circuo"))
+pi=3.14
+area_del_circulo = pi*radio*radio
+print(area_del_circulo)
+
+x = int(input("me podría dar usted el valor asignado de la x"))
+m = 2
+y = m*x - 2
+
+
+x=2
+y=2
+m= (y*2 - y*1)/(x*2 - x*1)
+
+import math
+X1 = 2
+X2 = 6
+Y1 = 2
+Y2 = 10
+d = math.sqrt(((x_2 - x_1)**2 + (y_2 - y_1)**2))
+print(d)
+
+print (m==d)
+x_3 = -3
+y = x_3**2 + 6*x_3 +9 
+print (y==0)
+
+
+
